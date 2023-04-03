fc4bfa9bafa7f0f1d6c641e36fbc34ac55a6a366
Yoon-gu Hwang <yz0624@gmail.com>
2023-04-03 11:20:15 +0900

    add initial code

---
+++ main01.py
@@ -0,0 +1,21 @@
+import mlflow
+import pandas as pd
+import click
+import logging
+
+@click.command()
+@click.argument("in_data")
+@click.argument("out_data")
+def main(in_data, out_data):
+    mlflow.log_param("in_data", in_data)
+    mlflow.log_param("out_data", out_data)
+
+    logging.info(f"Reading data from {in_data}")
+    df = pd.read_csv(in_data)
+    df.duration = df.duration + 10
+    
+    logging.info(f"Writing data to {out_data}")
+    df.to_csv(out_data, index=False)
+
+if __name__ == "__main__":
+    main()
